3538e9a0cd8a56c1f7601e9524698ecc59009a6d
Bruno Agostini <bagostini@daitangroup.com>
2016-04-27 14:38:02 -0300

    . Fixed transparency issues with i3wm

--- init.sh
+++ init.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+sudo rm -f ~/.i3/*
+sudo ln -sf ~/i3wm/config ~/.i3/.
+
 sudo apt-get install -y feh
+
+sudo apt-get install -y compton
+compton -cCGfF -b -i 0.5 --vsync opengl
+
+i3-msg restart
