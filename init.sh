1e1edbd2ee4ad4fbd7a571de925053752454a13c
Bruno Agostini <brunogsa.92@gmail.com>
2016-04-28 02:38:12 -0300

    . Fixed status bar. Now pretty . Updated init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 sudo rm -f ~/.i3/*
 sudo ln -sf ~/i3wm/config ~/.i3/.
+sudo ln -sf ~/i3wm/.conkyrc ~/.
 
+# For loading your wallpaper
 sudo apt-get install -y feh
 
-sudo apt-get install -y compton
+# Better application launcher
+sudo apt-get install -y gnome-do gnome-do-plugins
+
+# Applets
+sudo apt-get install -y nm-applet
+sudo apt-get install -y volti
 
 i3-msg restart
