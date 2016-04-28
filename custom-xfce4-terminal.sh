059e27e748d7b63abc0d3ef59bf7efc9eceb51af
Bruno Agostini <bagostini@daitangroup.com>
2016-04-28 15:47:54 -0300

    . Fixed a script that runs xfce4-terminal from the current path . Removed unnecessary apps, added compton

---
+++ custom-xfce4-terminal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xfce4-terminal --default-working-directory $(pwd) -e tmux
--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 sudo rm -f ~/.i3/*
 sudo ln -sf ~/i3wm/config ~/.i3/.
-sudo ln -sf ~/i3wm/.conkyrc ~/.
+
+# Install a lightweight terminal
+sudo apt-get install -y xfce4-terminal
 
 # For loading your wallpaper
 sudo apt-get install -y feh
+
+# For handling transparency
+sudo apt-get install -y compton
 
 # Better application launcher
 sudo apt-get install -y gnome-do gnome-do-plugins
